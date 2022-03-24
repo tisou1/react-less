fa467dba6b6c382335a863d45165df49e0f4244d
siry <taowang@pawnfii.com>
2022-03-24 18:22:23 +0800

    chore: add booleaner

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import {  configureStore, Store } from '@reduxjs/toolkit'
-import counterReducer from './reducers/counterSlice'
-
+// import counterReducer from './reducers/counterSlice'
+import { 
+  counterReducer,
+  booleanSlice
+} from './reducers'
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    booleaner: booleanSlice
   }
 })
 
