8ca441ca498bfbc2b4797bbad0423a117ac1f171
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-09-18 11:23:02 +0800

    fix: dark mode not working when refreshed

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,25 +1,32 @@
 declare global {
-  interface Window { __theme: any; }
+  interface Window { __theme: any }
 }
-//react beta
-window.__theme = 'light'
-function setTheme(newTheme:string) {
+// react beta
+const __theme = localStorage.getItem('theme')
+if (!__theme)
+  window.__theme = 'light'
+
+else if (__theme === 'dark')
+  setTheme('dark')
+
+function setTheme(newTheme: string) {
   console.log(newTheme)
   window.__theme = newTheme
-  if(newTheme === 'dark') {
+  if (newTheme === 'dark')
     document.documentElement.classList.add('dark')
-  } else {
+
+  else
     document.documentElement.classList.remove('dark')
-  }
 }
 
 function __changeTheme(newTheme: string) {
   setTheme(newTheme)
-  try{
-    localStorage.setItem('theme',newTheme)
-  } catch(err){}
+  try {
+    localStorage.setItem('theme', newTheme)
+  }
+  catch (err) {}
 }
 
 export {
-  __changeTheme
+  __changeTheme,
 }
